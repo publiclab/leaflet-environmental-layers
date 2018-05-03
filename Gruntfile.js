85f807f63da77dcf2040803ce502d46c710e245b
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2018-05-03 11:14:31 +0530

    Gruntfile added with plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,55 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-       
+        watch: {
+          files: ['<%= jshint.files %>'],
+          tasks: ['jshint']  // test file can be added here
+        },
+
+        browserify: {
+            dist: {
+                src: ['node_modules/jquery/dist/jquery.min.js', 'node_modules/leaflet/dist/leaflet.js', 'src/leafletEnvironmentalLayers.js'],
+                dest: 'dist/LeafletEnvironmentalLayers.js'
+            }
+        },
+
+        jasmine: {
+          src: "src/client/js/*.js",
+          options: {
+            specs: "spec/javascripts/*spec.js",
+            vendor: ['node_modules/jquery/dist/jquery.js','dist/LeafletEnvironmentalLayers.js','node_modules/jasmine-jquery/lib/jasmine-jquery.js'],
+          }
+        },
+
+        jshint: {
+            // define the files to lint
+          files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            // configure JSHint (documented at http://www.jshint.com/docs/)
+          options: {
+            // more options here if you want to override JSHint defaults
+            globals: {
+             jQuery: true,
+             console: true,
+             module: true
+            }
+          }
+        },
 
 
     });
 
-    
+    /*
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    *?
+
+    /* Default (development): Watch files and build on change. */
+    grunt.registerTask('build', [
+        'browserify:dist'
+    ]);
+    grunt.registerTask('test', ['jshint', 'jasmine']);
+    grunt.registerTask('build', ['browserify']);
 };
