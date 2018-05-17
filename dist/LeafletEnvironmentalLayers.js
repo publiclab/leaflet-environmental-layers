e254035991fd739436758f6ddad1481be41ab110
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2018-05-17 16:20:20 +0530

    Indentation done and comments removed

--- dist/LeafletEnvironmentalLayers.js
+++ dist/LeafletEnvironmentalLayers.js
@@ -24190,13 +24190,12 @@ require('jquery') ;
 require('leaflet') ; 
 
 
-
 L.Icon.SkyTruthIcon = L.Icon.extend({
     options: {
       iconUrl: 'https://www.clker.com/cliparts/T/G/b/7/r/A/red-dot.svg',
-      iconSize:     [30, 20], // size of the icon
-      iconAnchor:   [20 , 0], // point of the icon which will correspond to marker's location
-      popupAnchor:  [-5, -5] // point from which the popup should open relative to the iconAnchor
+      iconSize:     [30, 20], 
+      iconAnchor:   [20 , 0], 
+      popupAnchor:  [-5, -5] 
     }
 });
 
@@ -24204,14 +24203,23 @@ L.icon.skyTruthIcon = function () {
     return new L.Icon.SkyTruthIcon();
 };
 
-
 L.LayerGroup.SkyTruthLayer = L.LayerGroup.extend(
+    {
+         options: {
+            url: 'https://alerts.skytruth.org/json?n=100',
+            clearOutsideBounds: true ,      
+        },
+        
+        initialize: function (options) {
+            options = options || {};
+            L.Util.setOptions(this, options);  
+            this._layers = {};  
+        },
+    }
 
 );
 
-/*
-    Creates a new SkyTruthLayerl layer.
-*/
+
 L.layerGroup.skyTruthLayer = function (options) {
     return new L.LayerGroup.SkyTruthLayer(options);
 };
--- src/skyTruthLayer.js
+++ src/skyTruthLayer.js
@@ -4,13 +4,12 @@ require('jquery') ;
 require('leaflet') ; 
 
 
-
 L.Icon.SkyTruthIcon = L.Icon.extend({
     options: {
       iconUrl: 'https://www.clker.com/cliparts/T/G/b/7/r/A/red-dot.svg',
-      iconSize:     [30, 20], // size of the icon
-      iconAnchor:   [20 , 0], // point of the icon which will correspond to marker's location
-      popupAnchor:  [-5, -5] // point from which the popup should open relative to the iconAnchor
+      iconSize:     [30, 20], 
+      iconAnchor:   [20 , 0], 
+      popupAnchor:  [-5, -5] 
     }
 });
 
@@ -18,28 +17,23 @@ L.icon.skyTruthIcon = function () {
     return new L.Icon.SkyTruthIcon();
 };
 
-
 L.LayerGroup.SkyTruthLayer = L.LayerGroup.extend(
     {
          options: {
             url: 'https://alerts.skytruth.org/json?n=100',
-            clearOutsideBounds: true ,       // clears the marker which are outside the current bound
+            clearOutsideBounds: true ,      
         },
-        /*
-            Create the layer group using the passed options.
-        */
+        
         initialize: function (options) {
             options = options || {};
-            L.Util.setOptions(this, options);  // this function merges the options to this.option
-            this._layers = {};  // hash map to check if a marker (at long / lat) is not already added to map , all markers are as value in this hash-map .
+            L.Util.setOptions(this, options);  
+            this._layers = {};  
         },
     }
 
 );
 
-/*
-    Creates a new SkyTruthLayerl layer.
-*/
+
 L.layerGroup.skyTruthLayer = function (options) {
     return new L.LayerGroup.SkyTruthLayer(options);
 };
