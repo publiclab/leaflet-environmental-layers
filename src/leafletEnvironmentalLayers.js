30239382f1a1ba53564c7e0a30ba64f133f78843
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2018-05-25 02:07:36 +0530

    Justice Map layer completed

--- src/leafletEnvironmentalLayers.js
+++ src/leafletEnvironmentalLayers.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 require('./skyTruthLayer.js') ; 
 require('./odorReportLayer.js') ;
 require('./mapKnitterLayer.js') ; 
 require('./toxicReleaseLayer.js') ;
 require('leaflet-providers') ;
 require('./openWeatherMapLayer.js') ;
+=======
+/* comment */
+// require('leaflet') ; 
+
+require('./skyTruthLayer.js') ; 
+require('leaflet-providers') ;
+>>>>>>> Justice Map layer completed
