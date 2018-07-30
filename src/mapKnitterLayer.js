339ff09861c8004cd3ac7e5c475f12d7192d6484
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2018-07-30 21:15:41 +0530

    mapknitter size changed

--- src/mapKnitterLayer.js
+++ src/mapKnitterLayer.js
@@ -2,10 +2,10 @@
     options: {
       iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
       shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      iconSize: [25, 41],
-      iconAnchor: [12, 41],
+      iconSize: [15, 31],
+      iconAnchor: [6, 31],
       popupAnchor: [1, -34],
-      shadowSize: [41, 41]
+      shadowSize: [10, 11]
     }
 });
 
