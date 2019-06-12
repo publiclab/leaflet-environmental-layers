add8623e47471bba6984ed8a677fa832034e17b3
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2019-06-12 16:47:11 +0530

    PLpeople layer added

---
+++ src/PLpeopleLayer.js
@@ -0,0 +1,56 @@
+//require('leaflet-blurred-location') ;
+//require('leaflet-blurred-location-display') ;
+
+L.LayerGroup.PLpeopleLayer = L.LayerGroup.extend(
+
+    {
+        options: {
+            url: 'https://publiclab.org/api/srch/nearbyPeople',
+            clearOutsideBounds: false ,
+        },
+
+        initialize: function (options) {
+            options = options || {};
+            L.Util.setOptions(this, options);
+            this._layers = {};
+        },
+
+        onAdd: function (map) {
+            // map.on('moveend', this.requestData, this);
+            // this._map = map;
+            // this.requestData();
+            this._map = map ;
+            this.blurred_options = {
+                map: this._map
+            }
+            this.BlurredLocation = new BlurredLocation(this.blurred_options);
+            this.locations = [[23.1 , 77.1]] ; // testing marker
+            this.options_display = {
+                blurredLocation: this.BlurredLocation,
+                locations: this.locations,
+                source_url: "https://publiclab.org/api/srch/nearbyPeople",
+                color_code_markers: false, // by default this is false .
+                style: 'both' // or 'heatmap' or 'markers' , by default is 'both'
+            }
+
+            this.blurredLocationDisplay = new BlurredLocationDisplay(this.options_display);
+        },
+
+        onRemove: function (map) {
+            //map.off('moveend', this.requestData, this);
+            // if(typeof map.spin === 'function'){
+            //   map.spin(false) ;
+            // }
+            // this.clearLayers();
+            this._layers = {} ;
+            this.blurredLocationDisplay.removeLBLD() ;
+            this.blurredLocationDisplay.removeLBLD() ;
+        },
+        
+    }
+);
+
+
+L.layerGroup.pLpeopleLayer = function (options) {
+    return new L.LayerGroup.PLpeopleLayer(options) ;
+};
