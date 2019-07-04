9f625505f860e3d08e997d278fe9860ef56d536a
Ananya Arun <ananya.arun@research.iiit.ac.in>
2019-07-05 03:14:22 +0530

    Language layer tests added

--- spec/javascripts/indigenousLanguagesLayers_spec.js
+++ spec/javascripts/indigenousLanguagesLayers_spec.js
@@ -7,4 +7,11 @@ describe("Testing for indigenous Language layer", function() {
   it("Basic Test", function () {
   expect(true).toBe(true);
   });
+
+  it("Checks if layer polygons are present", function () {
+  // There should be atleast one polygon in the Language layer always .
+  expect($("#map").children()[0].childNodes[2].childNodes[0].childNodes[0].length).toBe(1) ;
+  });
+
+
 });
