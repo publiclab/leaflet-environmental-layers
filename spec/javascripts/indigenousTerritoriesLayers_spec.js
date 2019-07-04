a7b6cd5188749e6fccd34715ccb31cb85958f2e1
Ananya Arun <ananya.arun@research.iiit.ac.in>
2019-07-05 03:15:14 +0530

    Territories layer tests added

--- spec/javascripts/indigenousTerritoriesLayers_spec.js
+++ spec/javascripts/indigenousTerritoriesLayers_spec.js
@@ -7,4 +7,10 @@ describe("Testing for indigenous Territories layer", function() {
   it("Basic Test", function () {
   expect(true).toBe(true);
   });
+
+  it("Checks if layer polygons are present", function () {
+  // There should be atleast one polygon in the Territories layer always .
+  expect($("#map").children()[0].childNodes[2].childNodes[0].childNodes[0].length).toBe(1) ;
+  });
+
 });
