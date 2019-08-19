753305387d45c83712a009cc73fed1784dbc5269
Ananya Arun <ananya.arun@research.iiit.ac.in>
2019-08-19 19:51:56 +0530

    [WIP] Test indigeneous layers (#222)
    * added fixture files
    
    * added test files
    
    * Language layer tests added
    
    * Treaties layer tests added
    
    * Territories layer tests added
    
    * minor fix in tests
    
    * added single polygon object in layers
    
    * tests working finally

---
+++ spec/javascripts/indigenousTreatiesLayers_spec.js
@@ -0,0 +1,16 @@
+describe("Testing for indigenous Treaties layer", function() {
+
+  beforeEach(function () {
+      loadFixtures('indigenousTreatiesLayers.html');
+    });
+
+  it("Basic Test", function () {
+  expect(true).toBe(true);
+  });
+
+  it("Checks if layer polygons are present", function () {
+  // There should be atleast one polygon in the Treaties layer always .
+  expect($("#map").children()[0].childNodes[2].childNodes[0].childNodes[0].childNodes.length).toBeGreaterThan(0);
+  });
+
+});
