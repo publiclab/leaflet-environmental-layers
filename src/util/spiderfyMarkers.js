fd9c3040ba73715e1408971f0d42fd10a204702a
Renisha Christie <christie.reni@gmail.com>
2019-10-10 19:42:49 +0530

    Create a file using the spiderfy plugin

---
+++ src/util/spiderfyMarkers.js
@@ -0,0 +1,14 @@
+omsUtil = function (map) {
+    var oms = new OverlappingMarkerSpiderfier(map);
+
+    oms.addListener('click', function(marker) {
+        map.openPopup();
+    });
+
+    oms.addListener('spiderfy', function(markers) {
+        map.closePopup();
+    });
+
+    console.log('util working');
+    return oms;
+}
