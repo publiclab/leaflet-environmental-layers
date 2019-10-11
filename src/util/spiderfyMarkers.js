e428c0266db71e5669285c9ea1bc51ee34b62ba0
Renisha Christie <christie.reni@gmail.com>
2019-10-11 12:45:08 +0530

    Pass in options to the spiderfier instance

--- src/util/spiderfyMarkers.js
+++ src/util/spiderfyMarkers.js
@@ -1,5 +1,5 @@
 omsUtil = function (map, options) {
-    var oms = new OverlappingMarkerSpiderfier(map);
+    var oms = new OverlappingMarkerSpiderfier(map, options);
 
     var popup = new L.Popup();
     oms.addListener('click', function(marker) {
