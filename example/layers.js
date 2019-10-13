ff5b714a686743ee9300da2dacf345d8a68924e9
Renisha Christie <christie.reni@gmail.com>
2019-10-13 20:58:46 +0530

    Display badge on the map

--- example/layers.js
+++ example/layers.js
@@ -229,5 +229,39 @@
     var leafletControl = new L.control.layers(baseMaps,overlayMaps);
     leafletControl.addTo(map);
     
+    // Display number of active overlay layers
+    var touched = false;
+    var activeOverlays = leafletControl.getActiveOverlayNames();
+    var badge = L.control.badge(activeOverlays.length, { position: 'topright' }).addTo(map);
 
-    
+    map.on('layeradd', function(ev) {
+      // Displays badge on page load
+      $(badge.getContainer()).css("display", "block");
+      if(touched) {
+        $(badge.getContainer()).css("display", "none");
+      }
+    }); 
+
+    map.on('overlayadd', function(ev) {
+      this.removeControl(badge);
+      $(badge.getContainer()).css("display", "none");
+      activeOverlays = leafletControl.getActiveOverlayNames();
+      badge = L.control.badge(activeOverlays.length, { position: 'topright' }).addTo(map);
+      $(badge.getContainer()).css("display", "none");
+    });
+
+    map.on('overlayremove', function(ev) {
+      this.removeControl(badge);
+      activeOverlays = leafletControl.getActiveOverlayNames();
+      badge = L.control.badge(activeOverlays.length, { position: 'topright' }).addTo(map);
+      $(badge.getContainer()).css("display", "none");
+    }); 
+
+    L.DomEvent.on(leafletControl.getContainer(), 'mouseover', function (ev) {
+      touched = true;
+      $(badge.getContainer()).css("display", "none");
+    });
+
+    L.DomEvent.on(leafletControl.getContainer(), 'mouseout', function (ev) {
+      $(badge.getContainer()).css("display", "block");
+    });
