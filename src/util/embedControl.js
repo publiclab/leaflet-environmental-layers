2c30403749616d6eb5ac8d2205b93156058f033a
crisner <christie.reni@gmail.com>
2019-12-18 17:26:26 +0530

    Clone demo page with embed pointing to the actual page

--- src/util/embedControl.js
+++ src/util/embedControl.js
@@ -35,7 +35,7 @@ L.Control.Embed = L.Control.extend({
 
   generateCode: function() {
     var currentHash = window.location.hash;
-    var path = window.location.pathname;
+    var path = window.location.pathname === '/example/embed.html' ? '/example/index.html' : window.location.pathname;
     var hostname = this.options.hostname;
     var code = '<iframe style="border:none;" width="100%" height="900px" src="//' + hostname + '/leaflet-environmental-layers' + path + currentHash +'"></iframe>';
     return code;
