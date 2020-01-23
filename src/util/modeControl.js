d9ced40adfcb48516485b1776b906d5553bc1084
crisner <christie.reni@gmail.com>
2020-01-23 16:44:42 +0530

    Refactor minimalMode control

--- src/util/modeControl.js
+++ src/util/modeControl.js
@@ -8,30 +8,64 @@ L.Control.MinimalMode = L.Control.extend({
     initialize: function(layersControl, options) {
       L.Util.setOptions(this, options);
       this._layersControl = layersControl;
-      this._modeBtnContainer = L.DomUtil.create('div', 'leaflet-control-mode leaflet-bar leaflet-control');
-      var link = L.DomUtil.create('a', 'leaflet-control-mode-link', this._modeBtnContainer);
-      link.href = '#';
-      link.title = 'displayMode'
-      link.setAttribute('role', 'button');
-      link.setAttribute('aria-labelledby', 'displayMode');
-      this._minimalModeIcon = L.DomUtil.create('i', 'fas fa-map-marker-alt', link);
-      L.DomEvent.disableClickPropagation(link);
-      L.DomEvent.on(link, 'click', L.DomEvent.stop);
-      L.DomEvent.on(link, 'click', this.toggleMode, this);
     },
   
     onAdd: function(map) {
       this._map = map;
       this._map._minimalMode = this.options.minimalMode;
+      this._modeBtnContainer = L.DomUtil.create('div', 'leaflet-control-mode leaflet-bar leaflet-control');
+      this._defaultMode = this._createButton('Default mode', 'fas fa-map-marker-alt', this.loadDefaultMode);
+      this._minimalMode = this._createButton('Minimal mode', 'far fa-dot-circle', this.loadMinimalMode);
+      this._updateDisabled();
       return this._modeBtnContainer;
     },
-  
-    toggleMode: function() {
-      var self = this;
-      this.options.minimalMode = !this.options.minimalMode;
-      this._minimalModeIcon.classList = this.options.minimalMode ? 'far fa-circle' : 'fas fa-map-marker-alt';
-      this._map._minimalMode = this.options.minimalMode;
 
+    _createButton: function(title, icon, fn) {
+      var link = L.DomUtil.create('a', 'leaflet-control-mode-link', this._modeBtnContainer);
+      link.href = '#';
+      link.title = title;
+      link.setAttribute('role', 'button');
+      link.setAttribute('aria-labelledby', title);
+      L.DomUtil.create('i', icon, link);
+      L.DomEvent.disableClickPropagation(link);
+      L.DomEvent.on(link, 'click', L.DomEvent.stop);
+      L.DomEvent.on(link, 'click', fn, this);
+      return link;
+    },
+
+    loadMinimalMode: function() {
+      if (L.DomUtil.hasClass(this._minimalMode, 'leaflet-disabled')) {
+        return;
+      }
+      this.options.minimalMode = true;
+      this._map._minimalMode = this.options.minimalMode;
+      this._reloadLayers();
+      this._updateDisabled();
+    },
+
+    loadDefaultMode: function() {
+      if (L.DomUtil.hasClass(this._defaultMode, 'leaflet-disabled')) {
+        return;
+      }
+      this.options.minimalMode = false;
+      this._map._minimalMode = this.options.minimalMode;
+      this._reloadLayers();
+      this._updateDisabled();
+    },
+
+    _updateDisabled: function() {
+      var className = 'leaflet-disabled';
+      if(this._map._minimalMode) {
+        L.DomUtil.removeClass(this._defaultMode, className);
+        L.DomUtil.addClass(this._minimalMode, className);
+      } else {
+        L.DomUtil.addClass(this._defaultMode, className);
+        L.DomUtil.removeClass(this._minimalMode, className);
+      }
+    },
+
+    _reloadLayers: function() {
+      var self = this;
       this._layersControl._layers.forEach(function(layerObj) {
         if (layerObj.overlay && self._layersControl._map.hasLayer(layerObj.layer)) {
           self._layersControl._map.removeLayer(layerObj.layer)
