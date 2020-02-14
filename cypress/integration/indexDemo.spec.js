a38b15150780bbbabc894f275aaa8e7feebcd16f
crisner <christie.reni@gmail.com>
2020-02-14 17:23:42 +0530

    Move hash test to a seperate file and increase delay

--- cypress/integration/indexDemo.spec.js
+++ cypress/integration/indexDemo.spec.js
@@ -1,13 +1,7 @@
 describe('Loads demo page', function() {
   it('Visits demo page', function() {
-    cy.openWindow('/example/index.html#lat=43.00&lon=-83.00&zoom=3&layers=Standard');
-  })
-
-  it('changes hash on map movement', function() {
-    cy.window().its('map').invoke('setView',[38.694, -97.921], 6)
-    cy.wait(2000).then(() => {
-      cy.hash().should('equal', '#lat=38.694&lon=-97.921&zoom=6&layers=Standard')
-    })
+        cy.openWindow('/example/index.html#lat=43.00&lon=-83.00&zoom=3&layers=Standard');
+        cy.wait(5000)
   })
 
   it('has search control', function() {
