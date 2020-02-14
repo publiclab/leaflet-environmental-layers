04a90139fbf70421547b72456f1326b2e08d0b97
crisner <christie.reni@gmail.com>
2020-02-14 12:45:26 +0530

    Add child window to prevent iframe conflict on hash change

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,29 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('openWindow', (url, features) => {
+    const w = Cypress.config('viewportWidth')
+    const h = Cypress.config('viewportHeight')
+    if (!features) {
+      features = `width=${w}, height=${h}`
+    }
+    console.log('openWindow %s "%s"', url, features)
+  
+    return new Promise(resolve => {
+      if (window.top.aut) {
+        console.log('window exists already')
+        window.top.aut.close()
+      }
+      // https://developer.mozilla.org/en-US/docs/Web/API/Window/open
+      window.top.aut = window.top.open(url, 'aut', features)
+  
+      // letting page enough time to load and set "document.domain = localhost"
+      // so we can access it
+      setTimeout(() => {
+        cy.state('document', window.top.aut.document)
+        cy.state('window', window.top.aut)
+        resolve()
+      }, 500)
+    })
+  })
