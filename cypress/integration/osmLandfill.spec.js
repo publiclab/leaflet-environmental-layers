412809bf1b50a2c764c65c727797943ae29a7f53
crisner <christie.reni@gmail.com>
2020-03-03 21:21:02 +0530

    Add tests for osm landfill layer and unearthing layer

---
+++ cypress/integration/osmLandfill.spec.js
@@ -0,0 +1,21 @@
+describe('OSM Landfill, Mines and Quarries layer', function() {
+  it('adds markers on click', function() {
+    cy.openWindow('/example/index.html#lat=42.7029&lon=-99.1509&zoom=11&layers=Standard')
+    cy.wait(300)
+    cy.window().then((win) => {
+      const stub = cy.stub(win.OSMLandfillMineQuarryLayer, 'requestData')
+      cy.get('#menu-osmLandfillMineQuarryLayer label').click({ force: true }).then(() => {
+        expect(stub).to.be.called
+      }) 
+    }) 
+  })
+
+  it('should have the layer name added to the hash', function() {
+    cy.hash().should('eq', '#lat=42.7029&lon=-99.1509&zoom=11&layers=Standard,osmLandfillMineQuarryLayer')
+  })
+
+  it('removes layer name from the hash when clicked again', function() {
+    cy.get('#menu-osmLandfillMineQuarryLayer label').click({ force: true })
+    cy.hash().should('eq', '#lat=42.7029&lon=-99.1509&zoom=11&layers=Standard')
+  })
+})  
