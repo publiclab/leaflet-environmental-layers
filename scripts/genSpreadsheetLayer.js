f2c9fa0926f92d99b77299d4d7f67922fd75e7c6
daemon1024 <barun1024@gmail.com>
2021-07-01 01:24:33 +0530

    refactor actual script into a new file
    seperated the function in the existing file so as to make it compatible with both action and manual script

--- scripts/genSpreadsheetLayer.js
+++ scripts/genSpreadsheetLayer.js
@@ -1,37 +1,41 @@
 const fs = require("fs");
 const path = require("path");
 
-function generateSpreadsheetLayer(layerData, layersPath, infoPath, confirm) {
+function generateSpreadsheetLayer(layerData, confirm) {
   let newSpreadsheetLayers, newLayerInfo;
-  fs.readFile(path.resolve(__dirname, layersPath), "utf8", (err, data) => {
-    if (err) {
-      console.log(`Error reading file from disk: ${err}`);
-    } else {
-      // parse JSON string to JSON object
-      let spreadsheetLayers = JSON.parse(data);
-      spreadsheetLayers.push({
-        name: layerData.name,
-        url: layerData.url,
-      });
-      newSpreadsheetLayers = spreadsheetLayers;
-      if (confirm) {
-        fs.writeFile(
-          "../src/spreadsheetLayers/layers.json",
-          JSON.stringify(spreadsheetLayers, null, 2),
-          "utf8",
-          (err) => {
-            if (err) {
-              console.log(`Error writing file: ${err}`);
-            } else {
-              console.log(`File is written successfully!`);
+  fs.readFile(
+    path.resolve(__dirname, "../src/spreadsheetLayers/layers.json"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.log(`Error reading file from disk: ${err}`);
+      } else {
+        // parse JSON string to JSON object
+        let spreadsheetLayers = JSON.parse(data);
+        spreadsheetLayers.push({
+          name: layerData.name,
+          url: layerData.url,
+        });
+        newSpreadsheetLayers = spreadsheetLayers;
+        if (confirm) {
+          fs.writeFile(
+            path.resolve(__dirname, "../src/spreadsheetLayers/layers.json"),
+            JSON.stringify(spreadsheetLayers, null, 2),
+            "utf8",
+            (err) => {
+              if (err) {
+                console.log(`Error writing file: ${err}`);
+              } else {
+                console.log(`File is written successfully!`);
+              }
             }
-          }
-        );
+          );
+        }
       }
     }
-  });
+  );
 
-  fs.readFile(path.resolve(__dirname, infoPath), "utf8", (err, data) => {
+  fs.readFile("../src/info.json", "utf8", (err, data) => {
     if (err) {
       console.log(`Error reading file from disk: ${err}`);
     } else {
@@ -68,12 +72,5 @@ function generateSpreadsheetLayer(layerData, layersPath, infoPath, confirm) {
   return { newSpreadsheetLayers, newLayerInfo };
 }
 
-exports.generateSpreadsheetLayer = (data) => generateSpreadsheetLayer(data);
-
-let layerData = JSON.parse(fs.readFileSync("input.json", "utf8"));
-generateSpreadsheetLayer(
-  layerData,
-  "../src/spreadsheetLayers/layers.json",
-  "../src/info.json",
-  true
-);
+exports.generateSpreadsheetLayer = (...data) =>
+  generateSpreadsheetLayer(...data);
