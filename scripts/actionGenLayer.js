e46dc2d9ac18925bf49fbc19b58a8905426cea08
daemon1024 <barun1024@gmail.com>
2021-07-01 23:48:57 +0530

    update action script

--- scripts/actionGenLayer.js
+++ scripts/actionGenLayer.js
@@ -1,9 +1,14 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const marked = require("marked");
+const { execSync } = require("child_process");
 const { generateSpreadsheetLayer } = require("./genSpreadsheetLayer");
 
 const commentPrefix = "@github-actions run";
+
+function exec(cmd) {
+  console.log(execSync(cmd).toString());
+}
 
 async function run() {
   const comment = github.context.payload.comment.body;
@@ -18,30 +23,37 @@ async function run() {
     if (token.type === "code") {
       if (token.lang === "json") {
         const layerData = JSON.parse(token.text);
-        await generateSpreadsheetLayer(
-          layerData,
-          "../src/spreadsheetLayers/layers.json",
-          "../src/info.json",
-          true
-        );
+        console.log(layerData);
+        generateSpreadsheetLayer(layerData, true);
+        const gitUserEmail = "github-actions[bot]@users.noreply.github.com";
+        const gitUserName = "github-actions[bot]";
+        const prBranchName = `new-gen-layer/${layerData.name}`;
+
+        const baseBranchName = github.context.payload.repository.default_branch;
+        exec(`git config --global user.email "${gitUserEmail}"`);
+        exec(`git config --global user.name "${gitUserName}"`);
+        exec(`git fetch --all`);
+        exec(`git checkout ${baseBranchName}`);
+        exec(`git checkout -b ${prBranchName}`);
+
+        exec("git status");
+        exec("git add .");
+        exec(`git commit -m "layer: new layer ${layerData.name}"`);
+        exec(`git push -fu origin ${prBranchName}`);
+
+        //Set token secret
+        // const githubClient = new github(githubToken)
+        // await githubClient.pulls.create({
+        //   base: baseBranchName,
+        //   head: prBranchName,
+        //   owner: context.repo.owner,
+        //   repo: context.repo.repo,
+        //   title: `new-layer: ${layerData.name}`,
+        //   body: `Ref ${github.context.payload.comment.url}`,
+        // });
       }
     }
   }
-  const gitUserEmail = "github-actions[bot]@users.noreply.github.com";
-  const gitUserName = "github-actions[bot]";
-  const prBranchName = `new-gen-layer/${layerData.name}`;
-
-  const baseBranchName = github.context.payload.repository.default_branch;
-  exec(`git config --global user.email "${gitUserEmail}"`);
-  exec(`git config --global user.name "${gitUserName}"`);
-  exec(`git fetch --all`);
-  exec(`git checkout ${baseBranchName}`);
-  exec(`git checkout -b ${prBranchName}`);
-
-  exec("git status");
-  exec("git add .");
-  exec(`git commit -m "layer: new layer ${layerData.name}"`);
-  exec(`git push -fu origin ${prBranchName}`);
 }
 
 run().catch((e) => {
--- scripts/genSpreadsheetLayer.js
+++ scripts/genSpreadsheetLayer.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 function generateSpreadsheetLayer(layerData, confirm) {
+  console.log("layerData: inside function call",layerData)
   let newSpreadsheetLayers, newLayerInfo;
   fs.readFile(
     path.resolve(__dirname, "../src/spreadsheetLayers/layers.json"),
@@ -35,40 +36,44 @@ function generateSpreadsheetLayer(layerData, confirm) {
     }
   );
 
-  fs.readFile("../src/info.json", "utf8", (err, data) => {
-    if (err) {
-      console.log(`Error reading file from disk: ${err}`);
-    } else {
-      // parse JSON string to JSON object
-      let layerInfo = JSON.parse(data);
-      layerInfo[layerData.name] = {
-        name: layerData.name,
-        url: "",
-        data: {
-          type: "",
-          disclaimer: "",
-        },
-        description: "",
-        layer_desc: layerData.description,
-        icon: "#cc12cc",
-      };
-      newLayerInfo = layerInfo;
-      if (confirm) {
-        fs.writeFile(
-          "../src/info.json",
-          JSON.stringify(layerInfo, null, 2),
-          "utf8",
-          (err) => {
-            if (err) {
-              console.log(`Error writing file: ${err}`);
-            } else {
-              console.log(`File is written successfully!`);
+  fs.readFile(
+    path.resolve(__dirname, "../src/info.json"),
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.log(`Error reading file from disk: ${err}`);
+      } else {
+        // parse JSON string to JSON object
+        let layerInfo = JSON.parse(data);
+        layerInfo[layerData.name] = {
+          name: layerData.name,
+          url: "",
+          data: {
+            type: "",
+            disclaimer: "",
+          },
+          description: "",
+          layer_desc: layerData.description,
+          icon: "#cc12cc",
+        };
+        newLayerInfo = layerInfo;
+        if (confirm) {
+          fs.writeFile(
+            path.resolve(__dirname, "../src/info.json"),
+            JSON.stringify(layerInfo, null, 2),
+            "utf8",
+            (err) => {
+              if (err) {
+                console.log(`Error writing file: ${err}`);
+              } else {
+                console.log(`File is written successfully!`);
+              }
             }
-          }
-        );
+          );
+        }
       }
     }
-  });
+  );
   return { newSpreadsheetLayers, newLayerInfo };
 }
 
